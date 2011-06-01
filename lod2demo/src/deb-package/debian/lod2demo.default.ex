dfd4f249a9a8a00d9158056a895b4ece9de53bf3
bert.VanNuffelen@gmail.com <bert.VanNuffelen@gmail.com@e66c6f43-84b3-7c16-afb9-1c69667d4fe5>
2011-06-01 09:09:41 +0000

    add debian package contruction based on debbuild.
    in order to build the debian package
    
    mvn package -Pdeb-pkg
    
    The current list of added files are to much for this package and will be cleaned later.
    
    
    A    lod2demo/src/deb-package
    A    lod2demo/src/deb-package/debian
    A    lod2demo/src/deb-package/debian/menu.ex
    A    lod2demo/src/deb-package/debian/control
    A    lod2demo/src/deb-package/debian/emacsen-remove.ex
    A    lod2demo/src/deb-package/debian/watch.ex
    A    lod2demo/src/deb-package/debian/manpage.xml.ex
    A    lod2demo/src/deb-package/debian/manpage.1.ex
    A    lod2demo/src/deb-package/debian/compat
    A    lod2demo/src/deb-package/debian/manpage.sgml.ex
    A    lod2demo/src/deb-package/debian/emacsen-startup.ex
    A    lod2demo/src/deb-package/debian/changelog
    A    lod2demo/src/deb-package/debian/docs
    A    lod2demo/src/deb-package/debian/emacsen-install.ex
    AM   lod2demo/src/deb-package/debian/rules
    A    lod2demo/src/deb-package/debian/README.source
    A    lod2demo/src/deb-package/debian/lod2demo.doc-base.EX
    A    lod2demo/src/deb-package/debian/README
    A    lod2demo/src/deb-package/debian/postinst.ex
    A    lod2demo/src/deb-package/debian/preinst.ex
    A    lod2demo/src/deb-package/debian/postrm.ex
    A    lod2demo/src/deb-package/debian/source
    A    lod2demo/src/deb-package/debian/source/format
    A    lod2demo/src/deb-package/debian/prerm.ex
    A    lod2demo/src/deb-package/debian/init.d.ex
    A    lod2demo/src/deb-package/debian/lod2demo.cron.d.ex
    A    lod2demo/src/deb-package/debian/lod2demo.default.ex
    A    lod2demo/src/deb-package/debian/copyright
    A    lod2demo/src/deb-package/debian/lod2demo.install
    A    lod2demo/src/deb-package/debian/README.Debian
    AM   lod2demo/src/deb-package/build_debpkg.sh
    M    lod2demo/pom.xml

---
+++ lod2demo/src/deb-package/debian/lod2demo.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lod2demo initscript
+# sourced by /etc/init.d/lod2demo
+# installed at /etc/default/lod2demo by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
