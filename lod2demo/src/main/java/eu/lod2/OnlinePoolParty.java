97cd3b94a463b2f8106b09870f464d2c5db17024
dev.semanticwebcompany@gmail.com <dev.semanticwebcompany@gmail.com@e66c6f43-84b3-7c16-afb9-1c69667d4fe5>
2011-09-16 14:22:59 +0000

    Changed to new LOD2 demo instance

--- lod2demo/src/main/java/eu/lod2/OnlinePoolParty.java
+++ lod2demo/src/main/java/eu/lod2/OnlinePoolParty.java
@@ -62,7 +62,7 @@ public class OnlinePoolParty extends CustomComponent
 
 	Embedded browser = new Embedded();
 	try { 
-	  	URL url = new URL("http://pilot1.poolparty.biz");
+	  	URL url = new URL("http://pilot5.poolparty.biz");
 		browser = new Embedded("", new ExternalResource(url));
 		browser.setType(Embedded.TYPE_BROWSER);
 		browser.setSizeFull();
