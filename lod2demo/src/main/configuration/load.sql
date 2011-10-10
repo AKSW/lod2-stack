d78394f8f9d7e090be9af0b58ddd634922156764
bert.VanNuffelen@gmail.com <bert.VanNuffelen@gmail.com@e66c6f43-84b3-7c16-afb9-1c69667d4fe5>
2011-10-10 13:29:38 +0000

    bump the revision number to 18
    M    lod2demo/src/deb-package/debian/changelog
    M    lod2demo/src/main/configuration/load.sql
    M    lod2demo/pom.xml

--- lod2demo/src/main/configuration/load.sql
+++ lod2demo/src/main/configuration/load.sql
@@ -1,1 +1,1 @@
-DB.DBA.TTLP_MT (file_to_string_output ('/var/share/lod2demo/configuration.nt'), '', 'http://localhost/lod2democonfiguration');
+DB.DBA.TTLP_MT (file_to_string_output ('/var/lib/lod2demo/configuration.nt'), '', 'http://localhost/lod2democonfiguration');
