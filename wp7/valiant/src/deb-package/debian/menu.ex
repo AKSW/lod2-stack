1cb492bf7253cc7ed2beeb66a8216634d510eb29
bert.van.nuffelen@tenforce.com <bert.van.nuffelen@tenforce.com@e66c6f43-84b3-7c16-afb9-1c69667d4fe5>
2012-02-13 11:48:56 +0000

    update the debian package organisation
    D    debian/menu.ex
    D    debian/watch.ex
    D    debian/emacsen-remove.ex
    D    debian/valiant.doc-base.EX
    D    debian/emacsen-startup.ex
    D    debian/emacsen-install.ex
    D    debian/valiant.cron.d.ex
    D    debian/valiant.default.ex
    D    debian/init.d.ex
    M    debian/copyright
    A  + debian/valiant.doc-base

--- wp7/valiant/src/deb-package/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(valiant):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="valiant" command="/usr/bin/valiant"
--- wp7/valiant/src/deb-package/debian/valiant.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the valiant package
-#
-0 4	* * *	root	[ -x /usr/bin/valiant_maintenance ] && /usr/bin/valiant_maintenance
