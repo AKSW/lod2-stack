a028ced0194e69898dc814856512fca994919c12
karel.kremer@tenforce.be <karel.kremer@tenforce.be@e66c6f43-84b3-7c16-afb9-1c69667d4fe5>
2013-08-26 07:06:36 +0000

    packaging fixes, configuration fixes

--- lod2statworkbench/src/deb-package/create_user.sql
+++ lod2statworkbench/src/deb-package/create_user.sql
@@ -16,3 +16,5 @@ DB.DBA.USER_CREATE_IF_NOT_EXISTS(in name varchar, in pwd varchar)
 
 user_create_if_not_exists('lod2statworkbench', 'PWDLOD2');
 update sys_users set u_group=0, u_sql_enable=1 where u_name='lod2statworkbench';
+
+-- user is not created if existed before. This way, the old password is retained. The user will keep his old password and the config files will not be updated as per debian spec
